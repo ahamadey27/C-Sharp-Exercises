92e025b7253379c7446048bbe6ef2e09d42139e6
Alex Hamadey <128324646+ahamadey27@users.noreply.github.com>
2025-08-19 11:45:09 -0400

    arrays 2D

--- Giraffe Academy/Arrays/Program.cs
+++ Giraffe Academy/Arrays/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        int[,] numberGrid = { //2D array...each comma is a new dimension 
+            {1, 2},
+            {3, 4},
+            {5, 6}
+        };
+
+        System.Console.WriteLine(numberGrid[1, 1]);
+    }
+}
--- Giraffe Academy/Arrays/obj/Debug/net8.0/Arrays.AssemblyInfo.cs
+++ Giraffe Academy/Arrays/obj/Debug/net8.0/Arrays.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Arrays")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5dc1c4e25d0a80b38b8af38ca55de418386e3284")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2e93655894cdf3d25222f9dba53cd8c7b740c645")]
 [assembly: System.Reflection.AssemblyProductAttribute("Arrays")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Arrays")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
